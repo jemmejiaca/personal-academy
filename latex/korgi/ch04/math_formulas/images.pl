5b1666f03165ea06a10123c31277a6ed7e646fca
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-12-14 12:24:00 -0500

    Add the sedgewick project

---
+++ latex/korgi/ch04/math_formulas/images.pl
@@ -0,0 +1,324 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/delta>0;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="45" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ \delta&gt;0$">|; 
+
+$key = q/displaystylesec^2(x)=1+tan^2(x).qedhere;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="172" HEIGHT="39" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img48.png"
+ ALT="$\displaystyle \sec^2(x)=1+\tan^2(x). \qedhere $">|; 
+
+$key = q/displaystyle=(a*a')*b;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="99" HEIGHT="37" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img36.png"
+ ALT="$\displaystyle = (a*a')*b$">|; 
+
+$key = q/displaystyleXoverset{*}{backsim}Y;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="57" HEIGHT="44" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img27.png"
+ ALT="$\displaystyle X \overset{*}{\backsim} Y
+$">|; 
+
+$key = q/displaystylea*(a'*b);MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="81" HEIGHT="37" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img35.png"
+ ALT="$\displaystyle a*(a'*b)$">|; 
+
+$key = q/displaystyleointlimits_{(0,0)}^{(1,1)}fcdotdalpha;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="78" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="$\displaystyle \oint\limits_{(0,0)}^{(1,1)} f\cdot d\alpha
+$">|; 
+
+$key = q/displaystyle{bmatrix}a_{11}&a_{12}&cdots&a_{1n}a_{21}&a_{22}&cdots&a_{2n}hdotsfo2n}vdots&vdots&ddots&vdotsa_{m1}&a_{m2}&cdots&a_{mn}{bmatrix};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="371" HEIGHT="114" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img22.png"
+ ALT="$\displaystyle \begin{bmatrix}
+a_{11} &amp; a_{12} &amp; \cdots &amp; a_{1n}\\\\
+a_{21} &amp; a...
+...&amp; \vdots &amp; \ddots &amp; \vdots\\\\
+a_{m1} &amp; a_{m2} &amp; \cdots &amp; a_{mn}
+\end{bmatrix}$">|; 
+
+$key = q/qedsymbol;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img47.png"
+ ALT="$ \qedsymbol$">|; 
+
+$key = q/displaystylebiggl(frac{1-n}{1+n}biggr)^{n+1};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="100" HEIGHT="65" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="$\displaystyle \biggl( \frac{1-n}{1+n} \biggr)^{n+1}
+$">|; 
+
+$key = q/lrcorner;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$ \lrcorner$">|; 
+
+$key = q/displaystyle(x_{2},,ldots,x_{m});MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="100" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img34.png"
+ ALT="$\displaystyle (x_{2}, \ldots,x_{m})$">|; 
+
+$key = q/0<|x-c|<delta;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="116" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$ 0&lt;\vert x-c\vert&lt;\delta$">|; 
+
+$key = q/displaystyle{=}_{e}b;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="32" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img39.png"
+ ALT="$\displaystyle {=}_{e} b$">|; 
+
+$key = q/E;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img41.png"
+ ALT="$ E$">|; 
+
+$key = q/G(EslashF);MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img46.png"
+ ALT="$ G(E/F)$">|; 
+
+$key = q/displaystylesum_{substack{j=ki=k-1}}^{n,m}c_{i}alpha_{j};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="78" HEIGHT="83" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$\displaystyle \sum_{\substack{j=k\ i=k-1}}^{n, m}c_{i}\alpha_{j}
+$">|; 
+
+$key = q/sigma;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img24.png"
+ ALT="$ \sigma$">|; 
+
+$key = q/displaystyle(a_1,a_2,ldots,a_{n});MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="116" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img33.png"
+ ALT="$\displaystyle (a_1,a_2,\ldots,a_{n})$">|; 
+
+$key = q/displaystylef(x,y)=ax+by.;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="140" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\displaystyle f(x,y)=ax+by.$">|; 
+
+$key = q/ulcorner;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$ \ulcorner$">|; 
+
+$key = q/displaystyleX:underrightarrow{text{x+y}}:Y;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="74" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="$\displaystyle X\:\underrightarrow{\text{x+y}}\:Y
+$">|; 
+
+$key = q/displaystylef_n(x)={cases}-x^{2}+n,&text{six<0ynespar},alpha+x,&text{six>0},x^{2},&text{enotroscasos.}{cases};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="318" HEIGHT="95" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="$\displaystyle f_n(x)=
+\begin{cases}
+-x^{2}+n, &amp; \text{si $x&lt;0$ y $n$ es par...
+... \alpha +x, &amp; \text{si $x&gt;0$},\\\\
+x^{2}, &amp; \text{en otros casos.}
+\end{cases}$">|; 
+
+$key = q/displaystyleX_ipreccurlyeqX_j;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="69" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$\displaystyle X_i \preccurlyeq X_j
+$">|; 
+
+$key = q/displaystyleint_a^bf(x),dx=lim_{|P|to0},sum_{i=0}^{n}f(overline{x_i}),Delta_ix;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="254" HEIGHT="68" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$\displaystyle \int_a^b f(x) dx=
+\lim_{\vert P\vert\to 0} \sum_{i=0}^{n} f(\overline{x_i}) \Delta_ix
+$">|; 
+
+$key = q/displaystyleXoverset{text{ind}}{=}Y;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="62" HEIGHT="51" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img28.png"
+ ALT="$\displaystyle X \overset{\text{ind}}{=} Y
+$">|; 
+
+$key = q/mathbb{C};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img30.png"
+ ALT="$ \mathbb{C}$">|; 
+
+$key = q/K;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="20" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img43.png"
+ ALT="$ K$">|; 
+
+$key = q/f(x,y)=ax+by;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="141" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ f(x, y)=ax+by$">|; 
+
+$key = q/|f(x)-L|<varepsilon;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="112" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$ \vert f(x)-L\vert&lt;\varepsilon$">|; 
+
+$key = q/FsubseteqKsubseteqE;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="94" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img44.png"
+ ALT="$ F\subseteq K\subseteq E$">|; 
+
+$key = q/a';MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="20" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img38.png"
+ ALT="$ a'$">|; 
+
+$key = q/displaystyleleft.frac{dy}{dx}right|_{x=b}=b+1;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="120" HEIGHT="59" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="$\displaystyle \left. \frac{dy}{dx} \right\vert _{x=b}=b+1
+$">|; 
+
+$key = q/displaystylenablaf(x,,y)=left(frac{partialf}{partialy},,frac{partialf}{partialy}right);MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="181" HEIGHT="59" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img32.png"
+ ALT="$\displaystyle \nabla f(x, y)=\left(\frac{\partial f}{\partial y}, \frac{\partial f}{\partial y}\right)
+$">|; 
+
+$key = q/displaystyleoverset{a}{underset{b}{W}};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="23" HEIGHT="55" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img26.png"
+ ALT="$\displaystyle \overset{a}{\underset{b}{W}}
+$">|; 
+
+$key = q/f:{ensuremath{mathbb{R}}}longrightarrow{ensuremath{mathbb{C}}};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="92" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img31.png"
+ ALT="$ f:{\ensuremath{\mathbb{R}}}\longrightarrow {\ensuremath{\mathbb{C}}}$">|; 
+
+$key = q/KmapstoG(EslashK);MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="113" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img45.png"
+ ALT="$ K\mapsto G(E/K)$">|; 
+
+$key = q/displaystyleleft(frac{1-n}{1+n}right)^{n+1};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="100" HEIGHT="65" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$\displaystyle \left( \frac{1-n}{1+n} \right)^{n+1}
+$">|; 
+
+$key = q/llcorner;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$ \llcorner$">|; 
+
+$key = q/urcorner;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="17" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$ \urcorner$">|; 
+
+$key = q/Omega;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img25.png"
+ ALT="$ \Omega$">|; 
+
+$key = q/displaystyle{bmatrix}0&i&-i&11&0&i&-1i&-1&0&-i{bmatrix}{bmatrix}x_1x_2x_3x_4{bmatrix};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="192" HEIGHT="105" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img21.png"
+ ALT="$\displaystyle \begin{bmatrix}
+0 &amp; i &amp; -i &amp; 1\\\\
+1 &amp; 0 &amp; i &amp; -1\\\\
+i &amp; -1 &amp; 0 &amp; -i
+\end{bmatrix} \begin{bmatrix}
+x_1 \ x_2 \ x_3 \ x_4
+\end{bmatrix}$">|; 
+
+$key = q/e;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img40.png"
+ ALT="$ e$">|; 
+
+$key = q/mathfrak{F};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="$ \mathfrak{F}$">|; 
+
+$key = q/displaystyle=c*b;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="54" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img37.png"
+ ALT="$\displaystyle = c*b$">|; 
+
+$key = q/displaystylesum_{i=0}^{NM}beta_i;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="49" HEIGHT="73" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$\displaystyle \sum_{i=0}^{NM} \beta_i
+$">|; 
+
+$key = q/varepsilon>0;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="44" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$ \varepsilon&gt;0$">|; 
+
+$key = q/mathbb{R};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img29.png"
+ ALT="$ \mathbb{R}$">|; 
+
+$key = q/F;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img42.png"
+ ALT="$ F$">|; 
+
+1;
+
